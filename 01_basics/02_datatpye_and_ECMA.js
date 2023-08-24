fef8431fd8665b6184df6f2707734c25f2be6170
Sanjiv Paul <86189749+sanjivpaul@users.noreply.github.com>
2023-08-24 06:45:17 +0000

    datatype and ECMA

---
+++ 01_basics/02_datatpye_and_ECMA.js
@@ -0,0 +1,33 @@
+"use strict" // treat all js code as newer version.
+
+// alert(3+3); // this will run only in browser not in nodejs
+
+console.log("Sanjiv");
+
+let name = "Sanjiv"; // string
+let age = 18; //number
+let isLoggedIn = false; // boolean
+
+/* DataTypes */
+
+// number => 2 to power 53, range of number
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => declared but value not assigned 
+// symbol => unique
+
+// object:
+    // => null
+    // =>function
+    // => array
+    // => jsObjects
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+console.log(typeof Array); //function
+
+
+
+
