57557c520edab7545000ec7136b982d441c8b15a
Sanjiv Paul <86189749+sanjivpaul@users.noreply.github.com>
2023-08-24 06:49:56 +0000

    operator comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1); // ture
+// console.log(2 >= 1);// ture
+// console.log(2 <= 1);//false
+// console.log(2 < 1);//false
+// console.log(2 == 1);//false
+// console.log(2 != 1);// ture
+
+// console.log("2" > 1); // true => because js convert this string to number (but this is not good practice)
+// console.log("02" > 1); // true => because js convert this string to number (but this is not good practice)
+
+// remember this is not good practice
+// console.log(null > 0); //false
+// console.log(null == 0);//false
+// console.log(null >= 0);//ture
+
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
